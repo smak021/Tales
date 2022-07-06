50b6dcd498c98b47ce36ad54585185bca4d1d1ea
Safeer <safeersfm@gmail.com>
2022-07-06 19:05:06 +0530

    Third Update: Moving towards Alpha (Currently, Ver. 1.0)

---
+++ app/src/main/java/com/example/tale/test/TestAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.tale.adapters
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.tale.R
+import com.example.tale.model.Tale
+import com.example.tale.model.UserDetails
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.auth.User
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.friends_row.view.*
+import java.util.zip.Inflater
+
+class TestAdapter() : RecyclerView.Adapter<TestAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.friends_row,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 15
+    }
+
+    class ViewHolder(ItemView: View):RecyclerView.ViewHolder(ItemView) {
+
+    }
+}
